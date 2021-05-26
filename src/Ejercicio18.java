394ce690fdaf41aefde43529c1a7d085287c2375
Cherry708 <aluser8241@ieselcaminas.org>
2021-05-26 19:54:00 +0200

    Ejercicio20 detalle checkbox

--- src/Ejercicio18.java
+++ src/Ejercicio18.java
@@ -7,11 +7,23 @@ import java.util.ArrayList;
 public class Ejercicio18 {
 
     private ArrayList<JCheckBox> listaCheckBoxGeneraciones;
+    //Ejercicio19
+    private String[] arrayNombrePokemons1Generacion = {"Bulbasaur","Ivysaur","Venusaur","Charmander","Charmaleon",
+            "Charizard","Squietle","Wartortle","Blastoise","Caterpie"};
+    private String[] arrayNombrePokemons2Generacion =
+            {"Chikorita","Bayleef","Meganium","Cyndaquil","Quilava","Typhlosion","Totodile",
+                    "Croconaw","Feraligatr","Sentret"};
+
+
 
     private JPanel pnlForm;
     private JComboBox cmbGeneraciones;
     private JPanel pnlMaestro;
     private JPanel pnlDetalle;
+    private JButton btnAceptar;
+
+    JCheckBox checkBoxGeneraciones;
+
 
     public Ejercicio18(){
         pnlDetalle.setLayout(new GridLayout(10,1));
@@ -23,19 +35,39 @@ public class Ejercicio18 {
         Debemos esperar a que un evento suceda para
         modificar su estado.
          */
+        /* Ejercicio 19 */
+
         cmbGeneraciones.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 pnlDetalle.removeAll();
-                if (cmbGeneraciones.getSelectedIndex()+1 == 2){
-                    dibujarPanelDetalle1(1);
-                } else if (cmbGeneraciones.getSelectedIndex()+1 == 3){
-                    dibujarPanelDetalle1(2);
+                if (cmbGeneraciones.getSelectedIndex() == 1){
+                    dibujarPanelDetalle1(arrayNombrePokemons1Generacion);
+                } else if (cmbGeneraciones.getSelectedIndex() == 2){
+                    dibujarPanelDetalle1(arrayNombrePokemons2Generacion);
                 } else if (cmbGeneraciones.getSelectedIndex() == 0){
                     pnlDetalle.removeAll(); //Elimina el contenido y ya no será seleccionable pero no borra la pantalla
                     pnlDetalle.repaint(); //Dibuja nada o lo que le suministres
                 }
                 pnlDetalle.doLayout();
+            }
+        });
+
+        //Ejercicio 20
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nombresSeleccionados = "";
+
+                for (int contador = 0; contador < listaCheckBoxGeneraciones.size(); contador++){
+                    if (listaCheckBoxGeneraciones.get(contador).isSelected()){
+                        nombresSeleccionados = listaCheckBoxGeneraciones.get(contador).getText().concat(" "+nombresSeleccionados);
+                    }
+                }
+
+                if (!nombresSeleccionados.isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Has marcado "+nombresSeleccionados);
+                }
             }
         });
     }
@@ -49,13 +81,14 @@ public class Ejercicio18 {
         frame.setVisible(true);
     }
 
-    public void dibujarPanelDetalle1(int index){
+    public void dibujarPanelDetalle1(/*Ejercicio 19*/String[] array){
         int checkBoxSize = 10;
         for (int contador = 0; contador < checkBoxSize; contador++) {
-            JCheckBox checkBoxGeneraciones = new JCheckBox();
-            checkBoxGeneraciones.setText("Generacion "+index+" - Pokemon " + (contador + 1));
+            checkBoxGeneraciones = new JCheckBox();
+            //Ejercicio 19
+            checkBoxGeneraciones.setText(array[contador]);
             pnlDetalle.add(checkBoxGeneraciones);
+            listaCheckBoxGeneraciones.add(checkBoxGeneraciones);
         }
-
     }
 }
